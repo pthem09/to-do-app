98c4ed34a27928de0e9863fb01d745d2d39f9d1f
Paul Them <->
2024-06-08 17:10:27 -0400

    removing unneeded folders

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Manage your tasks</h1>
       </header>
     </div>
   );
