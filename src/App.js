e84008c49884b6ee2aa734a22994e7ce7d11896d
Paul Them <->
2024-06-08 18:07:54 -0400

    creating form and list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
+import ToDoForm from './Components/ToDoForm/ToDoForm';
+import ToDoList from './Components/ToDoList/ToDoList';
+
 export default function App() {
   return (
     <div className="App">
       <header>
         <h1>Manage your tasks</h1>
       </header>
+      <main>
+        <ToDoForm />
+        <ToDoList />
+      </main>
     </div>
   );
 }
---
+++ src/Components/ToDoForm/ToDoForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ToDoForm() {
+    return (
+        <div>ToDoForm</div>
+    )
+}
