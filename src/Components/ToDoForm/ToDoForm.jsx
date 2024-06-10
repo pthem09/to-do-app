275d86c0c505436301a761ef403e739cdbe13357
Paul Them <->
2024-06-10 16:10:21 -0400

    adding more form elements

--- src/Components/ToDoForm/ToDoForm.jsx
+++ src/Components/ToDoForm/ToDoForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Form, Button } from 'reactstrap';
+import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
 
 export default function ToDoForm() {
     return (
         <Form>
+            <FormGroup>
+                <Label htmlFor="link-date">Date</Label>
+                <Input
+                    type="date"
+                    name="date"
+                    id="link-data"
+                    required
+                />
+            </FormGroup>
             <Button type="submit">
                 Submit
             </Button>
